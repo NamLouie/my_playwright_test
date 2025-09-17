b384520a3f2aaa0f576a1e45c0b2f33627b3cfa6
Louie <louiesumande08@gmail.com>
2025-09-17 17:03:23 +0800

    test: add e2e test for add-to-cart flow with checkout details

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,10 +1,52 @@
 const { test, expect } = require('@playwright/test');
 
 
-test('ClientApp', async ({ page }) => {
+test('Client App Login', async ({ page }) => {
+
+    const email = "john.doe29@example.com";  
+    const product = page.locator('.card-body');
+    // const nameCard = "John Doe";
+    const cardNumber = "4542 9931 9292 2293";
     await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
-    await page.locator('#userEmail').fill('john.doe29@example.com');
+    await page.locator('#userEmail').fill(email);
     await page.locator('#userPassword').fill('StrongPass123!');
     await page.locator('#login').click();
-    await page.waitForLoadState('networkidle');;
+    await page.waitForLoadState('networkidle');
+    await page.locator('.card-body b').first().waitFor();
+    const count = await product.count();
+    for (let i = 0; i < count; ++i) {
+        if (await product.nth(i).locator('b').textContent() === 'ZARA COAT 3') {
+            await product.nth(i).locator('text= Add To Cart').click();
+            break;
+        }
+    }
+
+    await page.locator('[routerlink*="cart"]').click();
+    await page.locator('div li').first().waitFor();
+    const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+    expect(bool).toBeTruthy();
+    await page.locator('text=Checkout').click();
+    await page.locator('[placeholder*="Country"]').pressSequentially("ind");
+    const dropdown = page.locator('.ta-results');
+    await dropdown.waitFor();
+    const optionsCount = await dropdown.locator('button').count();
+    for (let i = 0; i < optionsCount; ++i) {
+        const text = await dropdown.locator('button').nth(i).textContent();
+        if (text === " India") {
+            await dropdown.locator('button').nth(i).click();
+            break;
+        }
+    }
+
+    await expect(page.locator('.user__name [type="text"]').first()).toHaveText(email);
+    await page.locator('.input.txt.text-validated').first().fill(cardNumber);
+    await page.locator('.input.ddl').first().selectOption('05');
+    await page.pause();
+
+
+    await page.locator('a:has-text("Place Order")').click();
+    await expect(page.locator('.hero-primary')).toHaveText(' Thankyou for the order. ');
+    const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
+    console.log(orderId);
+
 });
