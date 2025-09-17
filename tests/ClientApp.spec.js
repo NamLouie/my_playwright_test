40d80e969723e5ca86ee63edef8979f5a2578542
Louie <louiesumande08@gmail.com>
2025-09-17 20:09:26 +0800

    test: orders functionality

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -50,13 +50,29 @@ test('Client App Login', async ({ page }) => {
 
     await page.locator('button[type="submit"]').click();
 
-    await page.waitForLoadState('networkidle');
-    await page.pause();
-
-
     await page.locator('a:has-text("Place Order")').click();
     await expect(page.locator('.hero-primary')).toHaveText(' Thankyou for the order. ');
     const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
     console.log(orderId);
+
+    await page.locator("button[routerlink*='myorders']").click();
+
+    await page.locator('tbody').waitFor();
+
+    const rows = await page.locator('tbody tr');
+
+    for(let i = 0; i < await rows.count(); ++i) {
+        const rowOrderId = await rows.nth(i).locator('th').textContent();
+        if(orderId.includes(rowOrderId)) {
+            await rows.nth(i).locator('button').first().click();
+            break;
+        }
+    }
+
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
+
+
 
 });
