3b44f4455ad52a8749c40e88d698f40280ceb993
Louie <louiesumande08@gmail.com>
2025-09-19 20:09:42 +0800

    debug: moreValidation test scripts

--- tests/moreValidation.spec.js
+++ tests/moreValidation.spec.js
@@ -8,17 +8,12 @@ test ("Pop up Validation", async ({page}) => {
     await page.locator("#hide-textbox").click();
     await expect(page.locator("#displayed-text")).toBeHidden();
     page.on('dialog', dialog => dialog.accept());
-    await page.locator("#alertbtn").click();
-
+    await page.locator("#confirmbtn").click();
     await page.locator("#mousehover").hover();
-
     const framesPage = page.frameLocator("#courses-iframe");
-    await framesPage.locator("li a[href*='lifetime-access']: visible").click();
+    await framesPage.locator("li a[href*='lifetime-access']:visible").click();
     const textCheck = await framesPage.locator(".text h2").textContent();
     console.log(textCheck.split(" ")[1]);
-
-
-
 
     
 });
