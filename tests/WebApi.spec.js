bf41bb015fe28f908c443821bb90d24d64581a69
Louie <louiesumande08@gmail.com>
2025-09-20 12:25:00 +0800

    test: created order response using API

--- tests/WebApi.spec.js
+++ tests/WebApi.spec.js
@@ -1,7 +1,9 @@
 const { test, expect, request } = require('@playwright/test');
 
 const LoginPayLoad = {userEmail: "john.doe29@example.com", userPassword: "StrongPass123!"};
+const orderPayLoad = {orders: [{country: "India", productOrderedId: "68a961459320a140fe1ca57a"}]};
 let token;
+let orderId;
 
 
 test.beforeAll( async () => {
@@ -10,62 +12,38 @@ test.beforeAll( async () => {
     const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", {data:LoginPayLoad})
     expect((loginResponse).ok()).toBeTruthy();
 
+    
+    //Login API
     const loginResponseJSON = await loginResponse.json();
     token = loginResponseJSON.token;
-    console.log(token)
+    console.log(token);
+
+
+    //Handle Order Creation API
+    const createOrderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order", {
+        data: orderPayLoad,
+        headers: {
+            "Authorization": token,
+            "Content-Type": "application/json"
+        },
+
+    })
+    const createOrderResponseJSON = await createOrderResponse.json();
+    console.log(createOrderResponseJSON);
+    orderId = createOrderResponseJSON.orders[0];
 
 });
 
 test('Place the order', async ({ page }) => {
 
-    
-
-    const email = "";  
-    const product = page.locator('.card-body');
-    const nameCard = "John Doe";
-    const cardNumber = "4542 9931 9292 2293";
-    const cvv = "999";
-
+    //Login API
     page.addInitScript(value => {
 
         window.localStorage.setItem('token' , value );
     }, token );
 
     await page.goto("https://rahulshettyacademy.com/client/");
-
-
-    await page.locator('.card-body b').first().waitFor();
-
-    await page.locator(".card-body").filter({hasText: "ZARA COAT 3"}).getByRole('button', {name: "Add To Cart"}).click();
-
-    await page.getByRole("listitem").getByRole('button', {name:"Cart"}).click();
-
-    await page.locator('div li').first().waitFor();
-    
-    await expect(page.getByText("ZARA COAT 3")).toBeVisible();
-
-    await page.getByRole("button", {name: "Checkout"}).click();
-    await page.getByPlaceholder('Select Country').pressSequentially("ind");
-    await page.getByRole("button", {name: "India"}).nth(1).click();
-
-    await page.locator('.input.txt.text-validated').first().fill(cardNumber);
-    await page.locator('.input.ddl').first().selectOption('01');
-    await page.locator('.input.ddl').last().selectOption('25');
-    await page.locator('input[type="text"]').nth(1).fill(cvv);
-
-    await page.locator('input[type="text"]').nth(2).fill(nameCard);
-    await page.locator('input[name="coupon"]').fill('BLACKFRIDAY');
-
-    await page.locator('button[type="submit"]').click();
-
-    await page.getByText("PLACE ORDER").click();
-    await expect(page.getByText("Thankyou for the order.")).toBeVisible();
-
-    const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
-    console.log(orderId);
-
     await page.getByRole('button',{name: "ORDERS"}).click();
-
     await page.locator('tbody').waitFor();
 
     const rows = await page.locator('tbody tr');
@@ -78,15 +56,10 @@ test('Place the order', async ({ page }) => {
         }
     }
 
-    // await page.locator('tbody tr', {has: page.locator("th", {hasText: orderId })}).locator('button').first().click();
-    
-
-    //find row of orderID
-    //match the orderID 
-    //click the same orderID button "view"
 
     const orderIdDetails = await page.locator(".col-text").textContent();
     expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    
 
 
 
